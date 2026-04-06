1c99e2ec47c970212cf15e790f7ac6cc78a99814
Abraão de Paula <84100271+kedoshim@users.noreply.github.com>
2026-04-06 13:58:18 -0300

    Make qr code image background transparent

--- host-server/src/services/qr_service.py
+++ host-server/src/services/qr_service.py
@@ -1,6 +1,7 @@
 import qrcode
 from qrcode.image.styledpil import StyledPilImage
 from qrcode.image.styles.moduledrawers import RoundedModuleDrawer
+from qrcode.image.styles.colormasks import SolidFillColorMask # <-- Import this
 
 def generate_qr_code_image(url: str):
     
@@ -16,8 +17,11 @@ def generate_qr_code_image(url: str):
     img = qr.make_image(
         image_factory=StyledPilImage,
         module_drawer=RoundedModuleDrawer(),
-        fill_color="black",
-        back_color="white"
+        # Use an RGBA color mask instead of string kwargs
+        color_mask=SolidFillColorMask(
+            back_color=(255, 255, 255, 0), # White with 0 Alpha = Transparent
+            front_color=(0, 0, 0, 255)     # Black with 255 Alpha = Solid
+        )
     )
 
     return img
