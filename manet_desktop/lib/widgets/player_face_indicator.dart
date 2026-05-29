ca09524c64e1fc40ca8a88bb964dc294f00a32e1
Abraão de Paula <84100271+kedoshim@users.noreply.github.com>
2026-05-29 12:55:04 -0300

    Improve button press face animation

--- manet_desktop/lib/widgets/player_face_indicator.dart
+++ manet_desktop/lib/widgets/player_face_indicator.dart
@@ -5,7 +5,7 @@ import 'package:flutter/material.dart';
 import '../models/player_face.dart';
 import '../theme/app_colors.dart';
 
-class PlayerFaceIndicator extends StatelessWidget {
+class PlayerFaceIndicator extends StatefulWidget {
   final PlayerFaceData face;
   final double size;
   final bool roundedSquare;
@@ -34,50 +34,134 @@ class PlayerFaceIndicator extends StatelessWidget {
   });
 
   @override
+  State<PlayerFaceIndicator> createState() => _PlayerFaceIndicatorState();
+}
+
+class _PlayerFaceIndicatorState extends State<PlayerFaceIndicator>
+    with TickerProviderStateMixin {
+  late AnimationController _squishController;
+  late AnimationController _popController;
+
+  double _capturedSquishAmount = 0.0;
+
+  @override
+  void initState() {
+    super.initState();
+    _squishController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 600),
+    );
+    _squishController.addListener(() => setState(() {}));
+
+    _popController = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    );
+    _popController.addListener(() => setState(() {}));
+
+    if (widget.pressed) {
+      _squishController.forward();
+    }
+  }
+
+  @override
+  void didUpdateWidget(covariant PlayerFaceIndicator oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    if (widget.pressed && !oldWidget.pressed) {
+      _popController.stop();
+      _squishController.forward(from: 0.0);
+    } else if (!widget.pressed && oldWidget.pressed) {
+      _capturedSquishAmount = Curves.easeOutCubic.transform(
+        _squishController.value,
+      );
+      _squishController.stop();
+      _squishController.value = 0.0;
+      _popController.forward(from: 0.0);
+    }
+  }
+
+  @override
+  void dispose() {
+    _squishController.dispose();
+    _popController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final borderRadius = roundedSquare
-        ? BorderRadius.circular(size * 0.24)
-        : BorderRadius.circular(size);
+    final borderRadius = widget.roundedSquare
+        ? BorderRadius.circular(widget.size * 0.24)
+        : BorderRadius.circular(widget.size);
+
+    double scaleModifier = 1.0;
+
+    if (widget.pressed) {
+      final squishAmount = Curves.easeOutCubic.transform(
+        _squishController.value,
+      );
+      // Max squish reduces scale by up to 45%
+      scaleModifier = 1.0 - (squishAmount * 0.45);
+    } else if (_popController.isAnimating) {
+      final startScale = 1.0 - (_capturedSquishAmount * 0.45);
+      // The more it squished, the bigger the peak expansion
+      final peakScale = 1.0 + 0.10 + (_capturedSquishAmount * 0.45);
+
+      if (_popController.value < 0.1) {
+        // Phase 1: Fast explosive expansion (first 10% of duration)
+        final t = _popController.value / 0.1;
+        scaleModifier =
+            startScale +
+            (peakScale - startScale) * Curves.easeOutCubic.transform(t);
+      } else {
+        // Phase 2: Slow elastic settle back to normal (last 90% of duration)
+        final t = (_popController.value - 0.1) / 0.9;
+        scaleModifier =
+            peakScale - (peakScale - 1.0) * Curves.elasticOut.transform(t);
+      }
+    }
+
+    final finalScale = widget.scale * scaleModifier;
 
     return Opacity(
-      opacity: opacity,
-      child: AnimatedScale(
-        scale: pressed ? scale * 0.82 : scale,
-        duration: Duration(milliseconds: pressed ? 60 : 240),
-        curve: pressed ? Curves.easeOut : Curves.elasticOut,
+      opacity: widget.opacity,
+      child: Transform.scale(
+        scale: finalScale,
         child: Transform.translate(
-          offset: Offset(translateX, translateY),
+          offset: Offset(widget.translateX, widget.translateY),
           child: Container(
-            width: size,
-            height: size,
+            width: widget.size,
+            height: widget.size,
             decoration: BoxDecoration(
-              color: face.color,
+              color: widget.face.color,
               borderRadius: borderRadius,
               border: Border.all(
-                color: borderColor ?? AppColors.textPrimary,
-                width: math.max(1.5, size * 0.04),
+                color: widget.borderColor ?? AppColors.textPrimary,
+                width: math.max(1.5, widget.size * 0.04),
               ),
             ),
             child: Center(
               child: Padding(
                 padding: EdgeInsets.symmetric(
-                  horizontal: size * 0.12,
-                  vertical: size * 0.14,
+                  horizontal: widget.size * 0.12,
+                  vertical: widget.size * 0.14,
                 ),
                 child: FittedBox(
                   fit: BoxFit.scaleDown,
                   child: Transform.translate(
-                    offset: Offset(faceTranslateX, faceTranslateY),
+                    offset: Offset(
+                      widget.faceTranslateX,
+                      widget.faceTranslateY,
+                    ),
                     child: Transform.rotate(
-                      angle: playerFaceRotationAngle(face.rotation),
+                      angle: playerFaceRotationAngle(widget.face.rotation),
                       child: Text(
-                        face.faceText,
+                        widget.face.faceText,
                         maxLines: 1,
                         softWrap: false,
                         textAlign: TextAlign.center,
                         style: TextStyle(
                           fontFamily: 'monomaniac',
-                          fontSize: size * 0.7,
+                          fontSize: widget.size * 0.7,
                           height: 0.9,
                           color: Colors.black.withValues(alpha: 0.8),
                         ),
